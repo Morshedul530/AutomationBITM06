9f7774d5cf053474129e2e9afd9cd4026ceb2db3
Morshedul <mshuvo530@gmail.com>
2022-06-15 12:00:05 +0600

    Navigation,Dropdown,Alert,Checkbox

---
+++ 3.WebElement/4.Navigation/navigation_demo.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+
+class Navigate():
+    def test_navigate(self):
+        base_url = 'https://opensource-demo.orangehrmlive.com/'
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.maximize_window()
+        driver.get(base_url)
+        print('Base title: ' + driver.title)
+
+        # Navigate
+        driver.get('https://google.com')
+        time.sleep(10)
+        print('After open google ' + driver.title)
+
+        # back
+        driver.back()       # Demo
+        time.sleep(5)
+        print('After back ' + driver.title)
+
+        # Forward
+        driver.forward()    # Google
+        time.sleep(5)
+        print('After forward ' + driver.title)
+
+        driver.find_element(By.NAME, 'q').send_keys("Automation")
+
+        # Refresh
+        driver.refresh()
+        time.sleep(5)
+
+        driver.close()
+
+
+test_obj = Navigate()
+test_obj.test_navigate()
+
+
